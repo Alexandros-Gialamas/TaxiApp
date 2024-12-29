a9ada11655b91361dcf81d42008037f7ff711069
Alexandros Gialamas <alexandros.p.gialamas@google.com>
2024-12-29 18:10:21 +0200

    error-handling-revise

--- app/src/main/java/com/alexandros/p/gialamas/taxiapp/presentation/ui/navigation/NavigationController.kt
+++ app/src/main/java/com/alexandros/p/gialamas/taxiapp/presentation/ui/navigation/NavigationController.kt
@@ -18,7 +18,9 @@ import kotlin.reflect.KType
 import kotlin.reflect.typeOf
 
 
-inline fun <reified T : Any> navigationCustomArgument(isNullable: Boolean = false): Pair<KType, CustomNavType<T>> {
+inline fun <reified T : Any> navigationCustomArgument(
+    isNullable: Boolean = false
+): Pair<KType, CustomNavType<T>> {
     val serializer: KSerializer<T> = serializer()
     return typeOf<T>() to CustomNavType(serializer, isNullable)
 }
